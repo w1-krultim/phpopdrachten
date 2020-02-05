cbaf57286660e109b5e37da38cbffca586ee861e
Tim Krul <tim.krul@edu-kw1c.nl>
2020-02-05 12:15:07 +0100

    opdracht 2.1

---
+++ h2/opdrachten/opdracht_2.1.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Tim
+ * Date:5-02-2020
+ * Time:11:55
+ * Filer: index.php
+ */
+?>
+<link href="sub/style.css" type="text/css" rel="stylesheet">
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="./h2/opdrachten/opdracht_2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="h2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <p>
+        <?php echo "hello world";?>
+    </p>
+</main>
+</body>
+
